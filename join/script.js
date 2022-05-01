244757e32216a32129634e10699bdb2fe7d08dd6
EliasSchaut <eschaut@outlook.de>
2022-05-01 18:23:18 +0200

    implement alert in join

---
+++ join/script.js
@@ -0,0 +1,38 @@
+
+const form = document.querySelector('#form_register')
+const alert = document.querySelector('#alert_error')
+const btn_submit = document.querySelector('#button_submit')
+const btn_loading = document.querySelector('#button_loading')
+
+if (new URLSearchParams(window.location.search).get('status') === 'error') {
+  alert.style.display = 'block'
+}
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    btn_submit.style.display = 'none'
+    btn_loading.style.display = 'block'
+
+    const formData = new FormData(form).entries()
+    try {
+        const res = await register(formData)
+
+        if (res.status === 'success') {
+            direct_success()
+        } else {
+            redirect_error()
+        }
+    } catch (err) {
+        console.log(err)
+        redirect_error()
+    }
+})
+
+function redirect_error() {
+    window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/") + 1) + '?status=error'
+}
+
+function direct_success() {
+    window.location.href = window.location.origin + '/wer-ist-dabei?status=success'
+}
+
--- js/include.js
+++ js/include.js
@@ -3,12 +3,9 @@ const tmp_path = '../../res/tmp/';
 
 function include_html() {
     const divs = document.getElementsByTagName("div")
-    console.log(divs)
     for (const div of divs) {
         const attr = div.getAttribute("include-html")
-        console.log(attr)
         if (attr) {
-            console.log(attr)
             const xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function () {
                 if (this.readyState === 4) {
