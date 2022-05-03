bc944411d8ba4de1f08c3690259b1f3530b79d9c
EliasSchaut <eschaut@outlook.de>
2022-05-03 15:33:39 +0200

    add modals in page join

--- join/script.js
+++ join/script.js
@@ -4,6 +4,11 @@ const alert = document.querySelector('#alert_error')
 const btn_submit = document.querySelector('#button_submit')
 const btn_loading = document.querySelector('#button_loading')
 
+let modal_bed_first_toggle = true
+const modal_need_bed = document.querySelector('#modal_need_bed')
+let modal_nerd_first_toggle = true
+const modal_nerd = document.querySelector('#modal_nerd')
+
 if (new URLSearchParams(window.location.search).get('status') === 'error') {
   alert.style.display = 'block'
 }
@@ -36,3 +41,18 @@ function direct_success() {
     window.location.href = window.location.origin + '/wer-ist-dabei?status=success'
 }
 
+function toggle_modal_need_bed() {
+    if (modal_bed_first_toggle) {
+        const modal = new bootstrap.Modal(modal_need_bed);
+        modal.show()
+        modal_bed_first_toggle = false
+    }
+}
+
+function toggle_modal_nerd() {
+    if (modal_nerd_first_toggle) {
+        const modal = new bootstrap.Modal(modal_nerd);
+        modal.show()
+        modal_nerd_first_toggle = false
+    }
+}
