4685e4ab5ae5cfd072a2183551ac72b61c7f639f
EliasSchaut <eschaut@outlook.de>
2022-05-05 15:58:45 +0200

    update api

--- res/js/api.js
+++ res/js/api.js
@@ -1,22 +1,20 @@
 
-const api_base = 'http://localhost:8080/bday/';
+const api_base = 'http://localhost:8099/bday/'
 
-function register(data) {
-  console.log(data)
-  return fetch(api_base + 'member', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data),
-    mode: 'no-cors'
-  })
+async function add_member(data) {
+    return fetch(api_base + 'member', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data
+    })
 }
 
 async function get_members() {
-  return await fetch(api_base + 'members')
+    return await fetch(api_base + 'members')
 }
 
 async function get_members_counter() {
-  return await fetch(api_base + 'members_count')
+    return await fetch(api_base + 'members_count')
 }
