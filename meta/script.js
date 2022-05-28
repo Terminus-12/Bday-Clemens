59e701fad39cf39fabb01b7675da3ec01610ea96
EliasSchaut <eschaut@outlook.de>
2022-05-28 14:31:50 +0200

    add script to meta

---
+++ meta/script.js
@@ -0,0 +1,5 @@
+alert("Achtung, du bist im Meta-Programm!\n\nDas Programm ist potenziell nicht für dich geeignet!\n\nFalls du dich nicht für das Meta-Programm angemeldet hast, verlasse diese Seite umgehend!");
+function reveal() {
+    document.getElementById("reveal").style.display = "none";
+    document.getElementById("secret").style.display = "block";
+}
