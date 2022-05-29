ef6e13cd630e174b0abb4cedb4cb2d6e0f12f3d9
EliasSchaut <eschaut@outlook.de>
2022-05-29 21:30:17 +0200

    fix display issues again

--- meta/script.js
+++ meta/script.js
@@ -1,6 +1,5 @@
 alert("Achtung, du bist im Meta-Programm!\n\nDas Programm ist potenziell nicht für dich geeignet!\n\nFalls du dich nicht für das Meta-Programm angemeldet hast, verlasse diese Seite umgehend!");
 function reveal() {
     document.getElementById("reveal-btn").style.display = "none";
-    document.getElementById("reveal-capture").style.display = "none";
-    document.getElementById("secret").style.display = "block";
+    document.getElementById("secret").style.display = "flex";
 }
